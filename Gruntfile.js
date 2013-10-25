ddeda4bcd7588db5af1fd89c09a5cad510b283f8
Josh Bertrand <jbinc1986@gmail.com>
2013-10-25 01:57:19 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'build/js/custom.js',
+        dest: 'htdocs/assets/js/custom.min.js'
+      }
+    },
+    less: {
+            development: {
+                options: {
+                    paths: ["build/less"]
+                },
+                files: {
+                    "htdocs/assets/css/style.css": "build/less/main.less"
+                }
+            }
+        },
+        watch: {
+            files: "build/less/*",
+            tasks: ["less"]
+        }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
